7100369f2f4820bf298b176a02e349b5d9cb3fd2
cuong20698 <52898543+cuong20698@users.noreply.github.com>
2020-05-05 21:29:21 +0700

    ok

--- src/app/admin-page/contain/typemission-page/typemission-page.component.ts
+++ src/app/admin-page/contain/typemission-page/typemission-page.component.ts
@@ -3,6 +3,7 @@ import { ApiService } from 'src/app/api.service';
 import { CookieService } from 'ngx-cookie-service';
 import * as CryptoJS from 'crypto-js';
 import { from } from 'rxjs';
+import { TypemissionModule } from 'src/app/models/typemission/typemission.module';
 
 @Component({
   selector: 'app-typemission-page',
@@ -10,22 +11,34 @@ import { from } from 'rxjs';
   styleUrls: ['./typemission-page.component.css']
 })
 export class TypemissionPageComponent implements OnInit {
+  listtypemission: TypemissionModule[];
+  name2: string="";
   name: string="";
-  apiKey: string;
-  decPassword:string = "CTS-Security";
+  apiKey: string = "admin";
+  //decPassword:string = "CTS-Security";
+  typemission: TypemissionModule;
   constructor(private apiService: ApiService, private cookieService: CookieService) { 
   
   }
   ngOnInit(): void {
-    this.Decrypt(this.cookieService.get('cookieLogin'));
+    //this.Decrypt(this.cookieService.get('cookieLogin'));
+    this.apiService.GetlistTypeMission().subscribe((data: TypemissionModule[])=>
+    {
+      this.listtypemission = data;
+    },
+    err => {
+      console.log(err);
+    }
+    )
   }
+ 
   createTypeMission() : void
   {
     var typemission = {
-      id_type: 123,
+      id_type: null,
       name_type_mission: this.name,
       status: true,
-      id_employee: 189212,
+      id_employee: 189239,
       date: new Date(),
     }
     this.apiService.createTypeMission(this.apiKey, typemission).subscribe(data => {
@@ -36,7 +49,19 @@ export class TypemissionPageComponent implements OnInit {
       console.log(err);
     })
   }
-  private Decrypt (encryptText : string) {  
+  deleteTypeMission(id: number) : void
+  {
+    this.apiService.deleteTypeMission(id,this.apiKey,this.typemission).subscribe(data =>{
+      console.log(data['message']);
+      this.ngOnInit();
+    })
+  }
+  editTypeMission(type: TypemissionModule) : void
+  {
+    this.apiService.editTypeMission(this.apiKey,type);
+    this.ngOnInit();
+  }
+  /*private Decrypt (encryptText : string) {  
     this.apiKey = CryptoJS.AES.decrypt(encryptText, this.decPassword.trim()).toString(CryptoJS.enc.Utf8);  
-  } 
+  }*/
 }
